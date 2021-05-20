f4eadefd55d10cb89ea403f4a7f40c3b637687ed
Lambert Tran <lb.tran648@gmail.com>
2021-05-19 23:23:06 -0700

    test

--- collectuserinfo/settings.py
+++ collectuserinfo/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # LOG_PATH = os.path.join(BASE_DIR, "logs/debug.log")
