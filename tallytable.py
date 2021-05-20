c3ffe8a64e259b602ca50774c76580fbdb791677
Lambert Tran <lb.tran648@gmail.com>
2021-05-19 22:13:19 -0700

    refactor

--- tallytable.py
+++ tallytable.py
@@ -1,12 +1,12 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-import os 
+import os
 
+ACCESS_KEY= os.environ['AWS_ACCESS_KEY_ID'] if os.environ['AWS_ACCESS_KEY_ID'] else None
+SECRET_KEY= os.environ['AWS_SECRET_ACCESS_KEY'] if os.environ['AWS_SECRET_ACCESS_KEY'] else None
+REGION= os.environ['AWS_DEFAULT_REGION'] if os.environ['AWS_DEFAULT_REGION'] else 'us-west-2'
 
-
-dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_DEFAULT_REGION'])
-
-
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, region_name=REGION)
 
 def add_entry (user_dict):
 
@@ -36,13 +36,12 @@ def add_entry (user_dict):
         },
         UpdateExpression= "SET %s = :b, %s = :o, %s = :t" %(col_browser, col_os, col_totusers),
         ExpressionAttributeValues = {
-            ':b': tallytable[user_dict['browser']], 
-            ':o': tallytable[user_dict['os']], 
+            ':b': tallytable[user_dict['browser']],
+            ':o': tallytable[user_dict['os']],
             ':t': tallytable['Totalusers']
-        }, 
+        },
         ReturnValues = 'UPDATED_NEW'
     )
 
 
     return tallytable
-     
