d7614bf0d9e8c463948687b63a2855e7a6575f8a
Lambert Tran <lb.tran648@gmail.com>
2021-05-22 22:33:55 -0700

    test

--- collectuserinfo/settings.py
+++ collectuserinfo/settings.py
@@ -120,13 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'collect_app/static'),
-    '/static/'
-]
 
 # LOG_PATH = os.path.join(BASE_DIR, "logs/debug.log")
 
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+mkdir -p /collectuserinfo/collect_app/static
+python3 manage.py collectstatic
 gunicorn collectuserinfo.wsgi --bind 0.0.0.0:8080 --workers 3
